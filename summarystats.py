b12580fce3b998868ee3fd44a8b106d0c9588ecf
Gerald Kanapathy <gkanapathy@users.noreply.github.com>
2022-01-04 13:54:40 -0800

    summary stats with panda

---
+++ summarystats.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#load and process data into a global structure
+titanic = pd.read_csv("https://raw.githubusercontent.com/pandas-dev/pandas/master/doc/data/titanic.csv")
+
+def mean(field="Age"):
+    return titanic[field].mean()
+
+def median(field="Age"):
+    return titanic[field].median()
+
+def percentile(field="Age", percentile=[.25, .5, .75]):
+    return titanic[field].quantile(percentile)
